45384868808a65070ac13a1cc0c07a0cd4face2c
Xavier Minnamon <xaffekt.dev@gmail.com>
2023-07-15 12:11:23 -0700

    update 0.12
    JS functionality

--- Dog.js
+++ Dog.js
@@ -4,6 +4,20 @@ class Dog {
     constructor(data) {
         Object.assign(this, data)
     }
+
+    getDogHtml() {
+        const {name, age, bio} = this
+        return `
+        <div class="dog-text-container">
+            <h2 class="dog-info-text">${name}, ${age}</h2>
+            <p class="dog-bio-text">${bio}</p>
+        </div>
+        `
+    }
+
+    getDogImgUrl() {
+        return this.avatar
+    }
 }
 
 export default Dog
--- index.js
+++ index.js
@@ -2,5 +2,32 @@
 import dogs from './data.js'
 import Dog from "./Dog.js"
 
-//.src = " new value ";
+
+const dogContainer = document.getElementById('dog-container') //html goes in here, change background as well
+const badgeImg = document.getElementById('badge-img') //.src = " images/badge-nope.png ";
+const nopeBtn = document.getElementById('nope-btn')
+const likeBtn = document.getElementById('like-btn')
+
+document.addEventListener("click", (e) => {
+    console.log('click')
+    console.log(e.target)
+    if(e.target.closest == likeBtn) {
+        console.log('like button has been clicked')
+    }
+
+
+})
+
+const dog = new Dog(dogs[2])
+
+function renderDogSection(dog) {
+    dogContainer.innerHTML = dog.getDogHtml()
+    dogContainer.style.background = `url(${dog.getDogImgUrl()})` 
+    dogContainer.style.backgroundSize = `cover`
+    dogContainer.style.backgroundPosition = `center`
+}
+
+renderDogSection(dog)
+
+
 
